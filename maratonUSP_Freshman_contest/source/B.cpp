20c1af7dc68361ddd8ce0afc2867817324101b51
Kaki021 <871047205@qq.com>
2022-12-01 01:18:53 +0800

    complate B

--- maratonUSP_Freshman_contest/source/B.cpp
+++ maratonUSP_Freshman_contest/source/B.cpp
@@ -1,32 +1,48 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
+vector<int> primes;
+
+bool isPrime(int n){ 
+    for(int i=2;i*i<=n;i++){
+        if(n%i==0){
+            return false;
+        }
+    }
+    return true;
+}
 
 int maxPrime(int n){
-    int fact=2;
-    while(fact<n&&n>1){
-        while(fact<n&&n%fact==0){
-	    n/=fact;
-	 }
-        fact++;
+   int ans=1;
+    for( auto x:primes){
+        while(n%x==0){
+            ans=max(ans,x);
+            n/=x;
+        }
     }
-    return n;
+    return max(n,ans);
 }
 
 int main(){
-    int n=0;
-    cin>>n;
-    string ans;
-    int maxFac=0;
-    for(int i=0;i<n;i++){
+   for(int i=2;i<10010;i++){
+        if(isPrime(i)){
+            primes.push_back(i);
+        }
+   }
+   int n=0;
+   cin>>n;
+   int maxFact=-1;
+   string ans="null";
+   for(int i=0;i<n;i++){
         string name;
-	int id;
-	cin>>name>>id;
-	int max_prime=maxPrime(id);
-	if(max_prime>maxFac){
-	    ans=name;
-	    maxFac=max_prime;
-	}
-    }
-    cout<<ans;
+        int id;
+        cin>>name>>id;
+        int max_prime=maxPrime(id);
+        if(max_prime>maxFact){
+            ans=name;
+            maxFact=max_prime;
+        }
+   }
+   cout<<ans<<endl;
 }
